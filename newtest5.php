ba81a93ea74ee30519a1586aa34295b144fe02bc
Timur <tima.kundikerov@mail.ru>
2026-03-17 20:13:17 +0400

    ronaldo

--- newtest5.php
+++ newtest5.php
@@ -2,14 +2,3 @@
 
 function welcomeUser($name, $gender, $age)
 {
-    if ($age<18) {
-        echo "Привет";
-    } else {
-        if ($gender === "мужской") {
-            echo"Здравствуйте, уважаемый $name";
-        }   
-        if ($gender === "женщина") {
-            echo"Здравствуйте, уважаемая $name";
-        }
-    }
-} echo welcomeUser("Дима","мужской", 17);
